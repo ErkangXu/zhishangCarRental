98ebc0a1c2e929d1341fa897b268ef651cfcdeae
Erkang Xu <erkangx@amazon.com>
2019-01-01 14:54:23 -0700

    Don't define the port used

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ set -e
 echo "Starting SSH ..."
 service ssh start
 
-gunicorn --bind=0.0.0.0:8000 --timeout 600 /home/site/wwwroot/ZhishangCarRental.wsgi
+gunicorn --bind=0.0.0.0 --timeout 600 /home/site/wwwroot/ZhishangCarRental.wsgi
