e7ec431e26239ddf54010db59bb982642f7b0351
Erkang Xu <erkangx@amazon.com>
2019-01-03 09:58:24 -0700

    Make apt-get installation non-interactive

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ set -e
 echo "Starting SSH ..."
 service ssh start
 
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install g++
-apt-get install unixodbc-dev
+apt-get -yp install g++
+apt-get -yp install unixodbc-dev
 
 gunicorn --bind=0.0.0.0:8000 --timeout 600 ZhishangCarRental.wsgi
