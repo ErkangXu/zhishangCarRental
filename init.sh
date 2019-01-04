c9d9ea5b8e1d33be614049f6e21846fc43a34c75
Erkang Xu <erkangx@amazon.com>
2019-01-04 11:32:29 -0700

    Don't install msodbcsql and gnupg2

--- init.sh
+++ init.sh
@@ -6,14 +6,14 @@ service ssh start
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
+#curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+#curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 apt-get update
 
-apt-get -y install msodbcsql
-apt-get -y install gnupg
-apt-get -y install gnupg2
+#apt-get -y install msodbcsql
+#apt-get -y install gnupg
+#apt-get -y install gnupg2
 
 apt-get -y install g++
 apt-get -y install unixodbc-dev
