3e2b30f10468165baa40c896301f95520dda2f1a
Erkang Xu <erkangx@amazon.com>
2019-01-04 15:18:38 -0700

    install unixodbc-dev right after msodbcsql17

--- init.sh
+++ init.sh
@@ -6,21 +6,14 @@ service ssh start
 
 export DEBIAN_FRONTEND=noninteractive
 
-#curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-#curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-apt-get update
-
-#apt-get -y install msodbcsql
 #apt-get -y install gnupg
 #apt-get -y install gnupg2
-
-apt-get -y install g++
-apt-get -y install unixodbc-dev
 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 apt-get -y install msodbcsql17
+apt-get -y install g++
+apt-get -y install unixodbc-dev
 
 gunicorn --bind=0.0.0.0:8000 --timeout 600 ZhishangCarRental.wsgi
