a9986be898225675798c81bbc96c7ff2952c9497
Erkang Xu <erkangx@amazon.com>
2019-01-04 15:31:23 -0700

    install gnupg again

--- init.sh
+++ init.sh
@@ -6,8 +6,7 @@ service ssh start
 
 export DEBIAN_FRONTEND=noninteractive
 
-#apt-get -y install gnupg
-#apt-get -y install gnupg2
+apt-get -y install gnupg
 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
