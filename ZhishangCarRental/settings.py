de7d3955fc5b0d07e3d1146898d261951dd69625
Erkang Xu <erkangx@amazon.com>
2019-01-03 23:46:56 -0700

    Change ODBC driver

--- ZhishangCarRental/settings.py
+++ ZhishangCarRental/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST':'cwensyk0vm.database.windows.net',
         'PORT':'1433',
         'OPTIONS': {
-            'driver': 'SQL Server Native Client 11.0',
+            'driver': 'ODBC Driver 13 for SQL Server',
             'MARS_Connection': 'True',
         }
     }
