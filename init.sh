f0195656af54b1d11501324ebef676d07a21b4b1
Erkang Xu <erkangx@amazon.com>
2019-01-04 09:55:55 -0700

    Install Microsoft ODBC Driver

--- init.sh
+++ init.sh
@@ -5,7 +5,14 @@ echo "Starting SSH ..."
 service ssh start
 
 export DEBIAN_FRONTEND=noninteractive
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
 apt-get update
+
+apt-get -y install msodbcsql
+
 apt-get -y install g++
 apt-get -y install unixodbc-dev
 
