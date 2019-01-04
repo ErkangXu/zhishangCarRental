6acea541d87a7ec1a59073e74585995d29754b9d
Erkang Xu <erkangx@amazon.com>
2019-01-04 16:11:46 -0700

    ACCEPT_EULA

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ apt-get install -y apt-transport-https
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-apt-get -y install msodbcsql17
+sudo ACCEPT_EULA=Y apt-get -y install msodbcsql17
 apt-get -y install g++
 apt-get -y install unixodbc-dev
 
